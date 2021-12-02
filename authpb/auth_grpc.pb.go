4c91538bf9692086f480c42707c25990f22d4c70
Kyle Nusbaum <kyle@datadog.com>
2021-12-02 15:38:39 -0600

    updating auth protocol

--- authpb/auth_grpc.pb.go
+++ authpb/auth_grpc.pb.go
@@ -19,6 +19,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type AuthClient interface {
 	Login(ctx context.Context, opts ...grpc.CallOption) (Auth_LoginClient, error)
+	Test(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Empty, error)
 }
 
 type authClient struct {
@@ -60,11 +61,21 @@ func (x *authLoginClient) Recv() (*AuthMsg, error) {
 	return m, nil
 }
 
+func (c *authClient) Test(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*Empty, error) {
+	out := new(Empty)
+	err := c.cc.Invoke(ctx, "/auth.Auth/Test", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuthServer is the server API for Auth service.
 // All implementations must embed UnimplementedAuthServer
 // for forward compatibility
 type AuthServer interface {
 	Login(Auth_LoginServer) error
+	Test(context.Context, *Empty) (*Empty, error)
 	mustEmbedUnimplementedAuthServer()
 }
 
@@ -74,6 +85,9 @@ type UnimplementedAuthServer struct {
 
 func (UnimplementedAuthServer) Login(Auth_LoginServer) error {
 	return status.Errorf(codes.Unimplemented, "method Login not implemented")
+}
+func (UnimplementedAuthServer) Test(context.Context, *Empty) (*Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Test not implemented")
 }
 func (UnimplementedAuthServer) mustEmbedUnimplementedAuthServer() {}
 
@@ -114,13 +128,36 @@ func (x *authLoginServer) Recv() (*AuthMsg, error) {
 	return m, nil
 }
 
+func _Auth_Test_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).Test(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/auth.Auth/Test",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).Test(ctx, req.(*Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Auth_ServiceDesc is the grpc.ServiceDesc for Auth service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
 var Auth_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "auth.Auth",
 	HandlerType: (*AuthServer)(nil),
-	Methods:     []grpc.MethodDesc{},
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "Test",
+			Handler:    _Auth_Test_Handler,
+		},
+	},
 	Streams: []grpc.StreamDesc{
 		{
 			StreamName:    "Login",
