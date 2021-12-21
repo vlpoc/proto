4ef07efc2cc717d42fbd659d26ac8bff618bfbe4
Kyle Nusbaum <kyle@datadog.com>
2021-12-21 17:59:04 -0600

    Add exec protocol

--- generate.sh
+++ generate.sh
@@ -3,6 +3,7 @@
 set -x
 
 protoc -I=. --go_out=. --go_opt=paths=source_relative  --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-	authpb/auth.proto
+	authpb/auth.proto \
+	exec/exec.proto
 
 #	ds/ds.proto \
